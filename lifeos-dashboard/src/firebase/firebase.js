4d429ed596276c7d0421fe84a84c284363a3001c
Debojyoti Mandal <debojyoti.mandal@gmail.com>
2026-07-29 15:39:47 +0530

    updated env variables

--- lifeos-dashboard/src/firebase/firebase.js
+++ lifeos-dashboard/src/firebase/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAiJMTQICGAK9MwyTP_3LbSEhv3_gFW8-Y",
-  authDomain: "lifeos-dashboard-e4319.firebaseapp.com",
-  projectId: "lifeos-dashboard-e4319",
-  storageBucket: "lifeos-dashboard-e4319.firebasestorage.app",
-  messagingSenderId: "163966255694",
-  appId: "1:163966255694:web:73dff49d6325f31dbbf91f",
-  measurementId: "G-GQ2E9HEZ9H"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
